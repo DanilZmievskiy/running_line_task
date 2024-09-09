3aaecaffe06c5e741da1be11c71c0a259b9eb26d
Danil Zmievskiy <44033128+DanilZmievskiy@users.noreply.github.com>
2024-09-09 17:59:52 +0300

    Update models.py

--- running_line/models.py
+++ running_line/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Request(models.Model):
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
